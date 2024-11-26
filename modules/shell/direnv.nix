b2c353c3518a965b021c7b899c1f505432117fb0
dcrlsn <dcarlson@viimed.com>
2024-11-26 12:13:53 -0500

    correcting a mistake

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -1,9 +1,13 @@
+{ vars, pkgs, ... }:
+
 {
-  programs.direnv = {
-    enable = true;
-    silent = false;
-    loadInNixShell = true;
-    nix-direnv.enable = true;
-#    enableZshIntegration = true;
+  home-manager.users.${vars.user} = {
+    programs.direnv = {
+      enable = true;
+      silent = false;
+      loadInNixShell = true;
+      nix-direnv.enable = true;
+      enableZshIntegration = true;
+    };
   };
 }
