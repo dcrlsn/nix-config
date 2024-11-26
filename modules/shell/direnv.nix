8e0b459ff5cb57a2cd630f5c703979012fb6c2dc
dcrlsn <c4r150n@gmail.com>
2024-11-26 12:17:18 -0500

    removing stray options

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -4,8 +4,6 @@
   home-manager.users.${vars.user} = {
     programs.direnv = {
       enable = true;
-      silent = false;
-      loadInNixShell = true;
       nix-direnv.enable = true;
       enableZshIntegration = true;
     };
