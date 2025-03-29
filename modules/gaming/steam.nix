8f2107a32f8076f067638aad5c560ad05ee48ce2
dcrlsn <c4r150n@gmail.com>
2025-03-28 22:07:44 -0400

    cleanup

---
+++ modules/gaming/steam.nix
@@ -0,0 +1,21 @@
+{ config, lib, system, pkgs, unstable, vars, ... }:
+
+{  
+  programs.steam.enable = true;
+
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 47984 47989 47990 48010 ];
+    allowedUDPPortRanges = [
+      { from = 47998; to = 48000; }
+      { from = 8000; to = 8010; }
+    ];
+  };
+}
