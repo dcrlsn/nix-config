dc3a16e78216a2f451ea3bdcf3d5f000e6ace8a2
dcrlsn <carlson.daniel.j@gmail.com>
2026-01-02 19:06:44 -0500

    orca wrapper

--- modules/programs/orca-slicer.nix
+++ modules/programs/orca-slicer.nix
@@ -4,27 +4,30 @@
 
 { pkgs, ... }:
 
+let
+  orca-slicer-wrapped = pkgs.symlinkJoin {
+    name = "orca-slicer-wrapped";
+    paths = [ pkgs.orca-slicer ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/orca-slicer \
+        --set __GLX_VENDOR_LIBRARY_NAME mesa \
+        --set __EGL_VENDOR_LIBRARY_FILENAMES /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json \
+        --set MESA_LOADER_DRIVER_OVERRIDE zink \
+        --set GALLIUM_DRIVER zink \
+        --set WEBKIT_DISABLE_DMABUF_RENDERER 1 \
+        --set VK_DEVICE_MEMORY_REPORT_EXTENSION_NAME VK_EXT_device_memory_report \
+        --set ZINK_DESCRIPTOR_MODE lazy \
+        --set MESA_SHADER_CACHE_DISABLE true \
+        --set MESA_GL_VERSION_OVERRIDE 4.6 \
+        --set MESA_GLSL_VERSION_OVERRIDE 460 \
+        --set GALLIUM_HUD_VISIBLE false
+    '';
+  };
+in
 {
   environment.systemPackages = with pkgs; [
-    (pkgs.writeShellScriptBin "orca-slicer" ''
-      # Driver overrides
-      export __GLX_VENDOR_LIBRARY_NAME=mesa
-      export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json
-      export MESA_LOADER_DRIVER_OVERRIDE=zink
-      export GALLIUM_DRIVER=zink
-      export WEBKIT_DISABLE_DMABUF_RENDERER=1
-      
-
-      # Zink memory stuff to suppress the massive amount of errors... Thanks nvidia
-      export VK_DEVICE_MEMORY_REPORT_EXTENSION_NAME=VK_EXT_device_memory_report
-      export ZINK_DESCRIPTOR_MODE=lazy
-      export MESA_SHADER_CACHE_DISABLE=true
-      export MESA_GL_VERSION_OVERRIDE=4.6
-      export MESA_GLSL_VERSION_OVERRIDE=460
-      export GALLIUM_HUD_VISIBLE=false
-      
-      exec ${pkgs.orca-slicer}/bin/orca-slicer "$@"
-    '')
+    orca-slicer-wrapped
     
     # GTK dependencies
     gtk3
