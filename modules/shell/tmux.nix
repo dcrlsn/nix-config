ec3105d60bd89ed13e13f1fd69a9a8e77a91c36e
dcrlsn <carlson.daniel.j@gmail.com>
2026-09-05 19:37:57 -0400

    starship tweaks

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -36,14 +36,13 @@ in
           # Tokyo Night Theme Colors
           set -g status-interval 1
           set -g status-style "bg=#${c.bg_dark},fg=#${c.fg}"
-          set -g status-left "#[bg=#${c.blue},fg=#${c.black},bold] #S #[bg=#${c.bg_dark},fg=#${c.blue}] "
-          set -g status-right "#[fg=#${c.gray},bg=#${c.bg_dark}] %Y.%m.%d #[fg=#${c.blue},bg=#${c.bg_dark}] %H:%M:%S #[bg=#${c.purple},fg=#${c.black},bold] #h "
+          set -g status-left ""
+          set -g status-right-length 100
+          set -g status-right "#[fg=#${c.gray},bg=#${c.bg_dark}]%Y.%m.%d  #[fg=#${c.blue},bg=#${c.bg_dark}]%H:%M:%S  #[fg=#${c.purple},bg=#${c.bg_dark}]#[fg=#${c.black},bg=#${c.purple},bold] #h #[fg=#${c.purple},bg=#${c.bg_dark}]"
 
-          set -g window-status-separator ""
-          set -g window-status-style "bg=#${c.bg_dark},fg=#${c.comment}"
-          set -g window-status-format " #I:#W#F "
-          set -g window-status-current-style "bg=#${c.bg_highlight},fg=#${c.blue},bold"
-          set -g window-status-current-format " #I:#W#F "
+          set -g window-status-separator " "
+          set -g window-status-format "#[fg=#${c.inactive},bg=#${c.bg_dark}]#[fg=#${c.fg_dark},bg=#${c.inactive}] #I #[fg=#${c.inactive},bg=#${c.bg_highlight}]#[fg=#${c.comment},bg=#${c.bg_highlight}] #W#{?window_zoomed_flag, 󰊓,} #[fg=#${c.bg_highlight},bg=#${c.bg_dark}]"
+          set -g window-status-current-format "#[fg=#${c.blue},bg=#${c.bg_dark}]#[fg=#${c.black},bg=#${c.blue},bold] #I #[fg=#${c.blue},bg=#${c.bg_highlight}]#[fg=#${c.fg},bg=#${c.bg_highlight},bold] #W#{?window_zoomed_flag, 󰊓,} #[fg=#${c.bg_highlight},bg=#${c.bg_dark}]"
 
           # Pane borders
           set -g pane-border-style "fg=#${c.inactive}"
